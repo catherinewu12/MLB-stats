06bd409896b5323818187191823a9571ba8c0d65
catherinewu12 <45218241+catherinewu12@users.noreply.github.com>
2018-12-17 13:53:36 -0500

    Update mlb.py

--- mlb.py
+++ mlb.py
@@ -4,53 +4,16 @@ import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup 
 
-'''
-my_url = 'https://www.newegg.ca/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=graphics+card&ignorear=0&N=-1&isNodeId=1'
-
-#open connection and grab page
-uClient = uReq(my_url)
-#store the data
-page_html = uClient.read()
-uClient.close()
-
-#parse html using soup
-page_soup = soup(page_html, "html.parser")
-#print(page_soup.h1)
-
-#print(page_soup.body.span)
-
-#grab products
-containers = page_soup.findAll("div",{"class":"item-container"})
-#print(len(containers))
-
-
-for contain in containers:
-    brand = contain.a.img['title']
-
-    #smething's messed...
-
-'''
-
+#ESPN site
 my_url = "http://www.espn.com/mlb/stats/batting/_/year/2018/seasontype/2"
 
+#grab the HTML from the site
 uClient = uReq(my_url)
-
 page_html = uClient.read()
 uClient.close()
 
+#Beautiful soup this thing
 page_soup = BeautifulSoup(page_html, "html.parser")
-
-for script in page_soup(["script","style"]):
-    script.extract()
-
-text = page_soup.get_text()
-
-lines = (line.strip() for line in text.splitlines())
-chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
-text = '\n'.join(chunk for chunk in chunks if chunk)
-
-#print(text)
-
 
 
 #finding the players and their stats
@@ -80,22 +43,9 @@ for i in range(len(playerName)):
     print(text)
 
 
-
 '''
 alrighty
 go for stats next
-avg and ops maybe
-
-stats = tableStats[2].findAll('td',{'align':'left'})
-team = stats[2]
-
-#strip it bro
-text = team.get_text()
-
-lines = (line.strip() for line in text.splitlines())
-chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
-text = '\n'.join(chunk for chunk in chunks if chunk)
-
-print(text)
+ops and slugg?
 '''
 
